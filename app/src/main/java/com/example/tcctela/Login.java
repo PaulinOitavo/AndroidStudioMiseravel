8328a0916d2fe39e39199e28593233a14e4cfefb
rm86626 <henriquent90@gmail.com>
2024-08-15 12:19:55 -0300

    atualuizadasso disgraça 2 o retrno

--- app/src/main/java/com/example/tcctela/Login.java
+++ app/src/main/java/com/example/tcctela/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
@@ -16,7 +17,26 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getSupportActionBar().hide();
+        Button perfil = findViewById(R.id.bt_entrar);
+
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Login.this, Perfil.class);
+
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
+
+//        getSupportActionBar().hide();
         IniciarComponentes();
 
         text_tela_cadastro.setOnClickListener(new View.OnClickListener(){
